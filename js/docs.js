9f6db2018343a3c187847dd2e025f99147140e87
Kevin Ngo <me@ngokevin.com>
2016-05-27 23:51:10 -0700

    Site updated: 2016-05-27 23:51:06

--- js/docs.js
+++ js/docs.js
@@ -1,44 +1,42 @@
 /* global $ */
-
 (function () {
 
-var anchorHeadingsSelector = 'h2[id], h3[id], h4[id], h5[id], h6[id]';
-
 var content = $('.content');
-if (content) {
-  content.addEventListener('click', function (e) {
-    var el = e.target;
-    if (el.matches && el.matches(anchorHeadingsSelector)) {
-      window.location.hash = '#' + el.id;
-    }
-  });
+if (!content) { return; }
 
-  document.body.addEventListener('keyup', function (e) {
-    if (document.activeElement !== document.body) { return; }
-    var left = e.keyCode === 37;
-    var right = e.keyCode === 39;
-    if (left) {
-      var linkLeft = $('.guide-link-left');
-      if (linkLeft) {
-        linkLeft.click();
-      } else {
-        try {
-          $('.subnav-link.current').closest('.section-subnav').previousElementSibling.previousElementSibling.querySelector('.subnav-item:last-child .subnav-link').click();
-        } catch (e) {
-        }
-      }
-    } else if (right) {
-      var linkRight = $('.guide-link-right');
-      if (linkRight) {
-        linkRight.click();
-      } else {
-        try {
-          $('.subnav-link.current').closest('.section-subnav').nextElementSibling.nextElementSibling.querySelector('.subnav-link').click();
-        } catch (e) {
-        }
+var anchorHeadingsSelector = 'h2[id], h3[id], h4[id], h5[id], h6[id]';
+content.addEventListener('click', function (e) {
+  var el = e.target;
+  if (el.matches && el.matches(anchorHeadingsSelector)) {
+    window.location.hash = '#' + el.id.toLowerCase();
+  }
+});
+
+document.body.addEventListener('keyup', function (e) {
+  if (document.activeElement !== document.body) { return; }
+  var left = e.keyCode === 37;
+  var right = e.keyCode === 39;
+  if (left) {
+    var linkLeft = $('.guide-link-left');
+    if (linkLeft) {
+      linkLeft.click();
+    } else {
+      try {
+        $('.subnav-link.current').closest('.section-subnav').previousElementSibling.previousElementSibling.querySelector('.subnav-item:last-child .subnav-link').click();
+      } catch (e) {
       }
     }
-  });
-}
+  } else if (right) {
+    var linkRight = $('.guide-link-right');
+    if (linkRight) {
+      linkRight.click();
+    } else {
+      try {
+        $('.subnav-link.current').closest('.section-subnav').nextElementSibling.nextElementSibling.querySelector('.subnav-link').click();
+      } catch (e) {
+      }
+    }
+  }
+});
 
 })();
